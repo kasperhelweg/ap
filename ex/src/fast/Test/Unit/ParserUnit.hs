6b8fac4dd4cac676e3333e602b9e4790d28b59bc
khj <kasperhelweg@gmail.com>
2014-10-30 18:07:50 +0100

    Add unittest and cleanup

---
+++ ex/src/fast/Test/Unit/ParserUnit.hs
@@ -0,0 +1,68 @@
+{-
+  FastParser Unit Tests
+
+  Date: Sep 20, 2012
+  Author: Kasper Helweg Jonassen <kasperhelweg@gmail.com>
+
+  compile example :
+  ghc -i../ -Wall -fwarn-incomplete-uni-patterns -fno-warn-unused-do-bind -o UNIT ./ParserUnit.hs
+-}
+
+import Lib.SimpleParse
+import Fast.Internal.Parser
+
+import Control.Monad(liftM)
+import Test.QuickCheck
+
+main :: IO ()
+main = do nameUS
+          nameUF
+
+          integerUS
+
+--quickCheck ((\s -> s == s) :: [Char] -> Bool)
+
+
+
+-------------- NAME -------------- 
+nameF input = return $ parse ( name >> eof ) input
+
+-- nameU Succes
+nameUS = do nameF "kas3_pe_r" >>= \n -> case n of
+                                         [] -> print "test failed"
+                                         _  -> print "test OK"
+            nameF "l" >>= \n -> case n of
+                                 [] -> print "test failed"
+                                 _  -> print "test OK"
+
+-- nameU Failure
+-------------- Base
+            -- Reserved
+nameUF = do nameF "class" >>= \n -> case n of
+                                     [] -> print "test OK"
+                                     _  -> print "test failed"
+            
+-------------- Edge
+            -- empty
+            nameF " " >>= \n -> case n of
+                                 [] -> print "test OK"
+                                 _  -> print "test failed"
+            nameF "" >>= \n -> case n of
+                                [] -> print "test OK"
+                                _  -> print "test failed"
+            -- not letter
+            nameF "_var" >>= \n -> case n of
+                                    [] -> print "test OK"
+                                    _  -> print "test failed"
+                                    
+            nameF "3var" >>= \n -> case n of
+                                    [] -> print "test OK"
+                                    _  -> print "test failed"
+
+-------------- INTEGER --------------
+                                    
+integerF input = return $ parse ( integer >> eof ) input
+
+integerUS = do integerF " 1234333332221119999998885757574839" >>= \n -> case n of
+                                                                         [] -> print "test failed"
+                                                                         _  -> print "test OK"
